76f58ea0aa5edc6d0e7b7770df716e4dbf4f50db
zaheerali34 <zaheerrahman0@gmail.com>
2023-07-30 18:21:36 -0700

    Project

---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+// let search = document.getElementById("#search");
+// let searchtbar = document.getElementsByClassName("search-bar");
+
+
+let cartbar = document.querySelector(".cart-bar");
+let cart = document.querySelector("#cart-icon").onclick = () =>{
+    cartbar.classList.toggle("active");
+}
